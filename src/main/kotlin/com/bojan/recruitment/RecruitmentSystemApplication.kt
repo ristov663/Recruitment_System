4bc978fcad8fef59baa76f19d37f5b22ce050171
ristov663 <ristovbojan663@gmail.com>
2026-03-15 21:08:04 +0100

    First commit

---
+++ src/main/kotlin/com/bojan/recruitment/RecruitmentSystemApplication.kt
@@ -0,0 +1,11 @@
+package com.bojan.recruitment
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RecruitmentSystemApplication
+
+fun main(args: Array<String>) {
+    runApplication<RecruitmentSystemApplication>(*args)
+}
---
+++ src/test/kotlin/com/bojan/recruitment/RecruitmentSystemApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.bojan.recruitment
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RecruitmentSystemApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
